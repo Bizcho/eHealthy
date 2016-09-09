450a6bae52aec4d6de7fb5c11a01669ce3d69639
Sergio Gonzalez <sergio.xga@gmail.com>
2016-09-08 18:25:36 -0600

    Just add user_type to devise user migration

--- db/migrate/20160908234024_devise_create_users.rb
+++ db/migrate/20160908234024_devise_create_users.rb
@@ -30,6 +30,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## Custom
+      t.string :user_type, default: 'user'
+
 
       t.timestamps
     end
